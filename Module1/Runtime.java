48c9343ccf346b5fbcd377f5a1ccfb500de6d2c8
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-04-06 16:41:27 +0530

    Fixed the erros in the multi inheritance

--- Module1/Runtime.java
+++ Module1/Runtime.java
@@ -23,8 +23,37 @@ class Runtime {
 
         Vehicle v1 = new Car();   // Parent reference, child object
         Vehicle v2 = new Bike();
-
+        
         v1.start();   // Calls Car's start()
         v2.start();   // Calls Bike's start()
     }
 }
+
+/* class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+Class Cat extends Animal{
+@Override
+void sound(){
+System.out.println("Cat Meows");
+}
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal pet = new Dog();  // runtime polymorphism
+        pet.sound();
+        Animal pet1 = new Cat();
+        pet.sound();
+    }
+} */
