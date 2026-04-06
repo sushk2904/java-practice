be9dab8e18c687558130926fe9d134e9053ccf10
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-04-06 16:43:16 +0530

    Learned the concept of multithreading in java

---
+++ Module5/Multithread.java
@@ -0,0 +1,29 @@
+package Module5;
+
+import java.util.*;
+
+
+class A extends Thread{
+    public void run(){
+        for(int i=1; i<=100; i++){
+            System.out.println("Hi");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for (int i=1; i<=100; i++){
+            System.out.println("Hello");
+        }
+    }
+}
+
+class Multithread {
+    public static void main(String args[]){
+        A obj1 = new A();
+        B obj2 = new B();
+        obj1.start();
+        obj2.start();
+    }
+}
