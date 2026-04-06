e3fb85075ab2e9396bc49a909dc8c4c16fc67a09
Sushant Kumar <unknownsushantone2904@gmail.com>
2026-04-06 16:45:43 +0530

    Learnt how runnable implements a class and then we can declare an object in the main class and then again use it as a thread to use the start function

---
+++ Module5/Runnablemethod.java
@@ -0,0 +1,44 @@
+package Module5;
+
+class A implements Runnable{
+    public void run(){
+        for(int i =1; i<=5; i++){
+            System.out.println("Hi");
+        }
+    }
+}
+
+class B implements Runnable{
+    public void run(){
+        for(int i =1; i<=5; i++){
+            System.out.println("Hello");
+        }
+    }
+}
+
+
+
+
+public class Runnablemethod {
+    public static void main(String args[]){
+        Runnable obj1 = new A();
+        Runnable obj2 = new B();
+
+        Runnable obj3 = () > 
+        {
+            public void run(){
+                for(int i =1; i<=5; i++){
+                System.out.println("Namaste");
+        }
+            
+
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        Thread t3 = new Thread(obj3);
+        
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+        }
